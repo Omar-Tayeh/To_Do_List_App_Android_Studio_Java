148f8fe09dc37f11d4d6cf671385751d9ff8bec5
Omar-Tayeh <tayeho@outlook.com>
2023-08-08 15:45:22 +0100

    Entity and Databse setup

--- app/src/main/java/com/tayeh/todolistapp/MainActivity.java
+++ app/src/main/java/com/tayeh/todolistapp/MainActivity.java
@@ -1,14 +1,33 @@
 package com.tayeh.todolistapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.tayeh.todolistapp.Helpers.DatabaseHelper;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private FloatingActionButton fab;
+    private DatabaseHelper myDb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerview);
+        fab = findViewById(R.id.fab);
+        myDb = new DatabaseHelper(MainActivity.this);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
